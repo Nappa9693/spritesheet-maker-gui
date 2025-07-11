43e8d0c0f939213b8646f03c826b789189e9d9d9
Nappa9693 <nerdyfreak9000@gmail.com>
2025-07-11 08:29:19 -0700

    Initial commit with basic electron skeleton

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // For Mac OS. Note activate listener is set here
+  // because it should not listened to until the app is
+  // initialized.
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// For Windows and Linux
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
