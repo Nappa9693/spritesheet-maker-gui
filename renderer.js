b8f9d04b144936a7f378e6b8ebc37a022994a6cb
Nappa9693 <nerdyfreak9000@gmail.com>
2025-07-11 13:24:20 -0700

    Added message modal

--- renderer.js
+++ renderer.js
@@ -12,10 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
     .addEventListener("click", async () => {
       await displayOpenFolderDialog("destinationDirectory");
     });
+  document
+    .getElementById("messageModalCloseButton")
+    .addEventListener("click", hideMessageModal);
+
+  window.spriteSheetMaker.onCreateSpriteSheetMessage(
+    onCreateSpriteSheetMessage,
+  );
 });
 
-function createSpriteSheet() {
+let processingSpriteSheetRequest = false;
+
+async function createSpriteSheet() {
+  if (processingSpriteSheetRequest) {
+    return;
+  }
   try {
+    processingSpriteSheetRequest = true;
     const sourceDirectory = document.getElementById("sourceDirectory").value;
     const destinationDirectory = document.getElementById(
       "destinationDirectory",
@@ -24,28 +37,51 @@ function createSpriteSheet() {
     const columnCount = parseInt(document.getElementById("columnCount").value);
 
     // TODO: Add validation.
-    console.log(sourceDirectory);
 
-    window.spriteSheetMaker.createSpriteSheet(
+    showMessageModal();
+    await window.spriteSheetMaker.createSpriteSheet(
       sourceDirectory,
       `${destinationDirectory}/${fileName}`,
       columnCount,
     );
-    window.spriteSheetMaker.onCreateSpriteSheetMessage(
-      onCreateSpriteSheetMessage,
-    );
   } catch (ex) {
-    // TODO: Display message modal.
-    console.log(ex);
+    addMessageToMessageModal(ex);
   }
+
+  processingSpriteSheetRequest = false;
 }
 
 function onCreateSpriteSheetMessage(message) {
-  // TODO: Set message in message modal.
-  console.log(message);
+  addMessageToMessageModal(message);
 }
 
 async function displayOpenFolderDialog(inputId) {
   const folderPath = await window.electronAPI.openFolder();
   document.getElementById(inputId).value = folderPath;
+}
+
+function showMessageModal() {
+  document.getElementById("messageModal").showModal();
+}
+
+function hideMessageModal() {
+  document.getElementById("messageModal").close();
+  resetMessageModal();
+}
+
+function addMessageToMessageModal(message) {
+  const messageModal = document.getElementById("messageModal");
+  const paragraphElement = document.createElement("p");
+
+  paragraphElement.innerText = message;
+  messageModal.appendChild(paragraphElement);
+}
+
+function resetMessageModal() {
+  const messageModal = document.getElementById("messageModal");
+  let paragraphElements = messageModal.querySelectorAll("p");
+
+  paragraphElements.forEach((element) => {
+    messageModal.removeChild(element);
+  });
 }
